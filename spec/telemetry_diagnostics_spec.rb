4262f8a9639722134e2157f86dfc5a66b6162ffe
Elizabeth Dalton <you@example.com>
2017-10-10 13:40:10 +0100

    final changes

--- spec/telemetry_diagnostics_spec.rb
+++ spec/telemetry_diagnostics_spec.rb
@@ -4,23 +4,34 @@ RSpec.describe TelemetryDiagnostics do
     describe "#check_transmission"  do
          context "with failing online status" do
              it "throws an error" do
-                tc = instance_double("TelemetryClient", :online_status => false)
+                tc = instance_double("TelemetryClient", 
+                  :online_status => false,
+                  :connect => nil,
+                  :disconnect => nil
+                )
                 allow(TelemetryClient).to receive(:new) {tc}
                 td = TelemetryDiagnostics.new 
                 
-                expect{td.check_transmission}.to raise_exception.with_message("Unable to connect.")
+                expect { td.check_transmission }.to raise_exception.with_message("Unable to connect.")
               end
          end
 
-        #  context "with passing online status" do
-        #     it "returns diagnostic info" do
-        #        td = TelemetryDiagnostics.new 
-        #        tc_2 = TelemetryClient.new
-        #        allow(TelemetryClient).to receive(:new) {tc_2}
-        #        allow(tc_2).to receive(:online_status) {true}
-        #        expect(td.check_transmission).to eq('hi')
-        #      end
-        # end
+         context "with passing online status" do
+            it "returns diagnostic info" do
+                tc = instance_double(
+                    "TelemetryClient", 
+                    :online_status => true,
+                    :connect => nil,
+                    :disconnect => nil,
+                    :send => "sent",
+                    :receive => "receive",
+                )
+              allow(TelemetryClient).to receive(:new) {tc}
+              td = TelemetryDiagnostics.new 
+
+               expect(td.check_transmission).to eq('receive')
+             end
+        end
     end
 
 end
